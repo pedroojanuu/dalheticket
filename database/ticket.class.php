31300d4a4f278a22eddbf5a2ac6cb7bc5d98f457
pedroojanuu <93834123+pedroojanuu@users.noreply.github.com>
2023-03-28 09:38:20 +0100

    databse/ticket.class.php written.

--- database/ticket.class.php
+++ database/ticket.class.php
@@ -1,5 +1,4 @@
 <?php
-
     declare(strict_types = 1);
 
     class Ticket {
@@ -9,11 +8,40 @@
         public string $status;
         public string $department;
 
-        public function __construct(int $id, string $client) {
+        public function __construct(PDO $db, int $id, string $client) {
             $this->id = $id;
             $this->client = $client;
             $this->status = 'Unsolved';
+
+            $stmt = $db->prepare('INSERT INTO Ticket VALUES (?)');
+            $stmt->execute(array($this->id, $this->client, null, $this->status, null));
+        }
+
+        public function setAgent(PDO $db, string $agent) {
+            $this->agent = $agent;
+
+            $stmt = $db->prepare('UPDATE Ticket SET agent = :agent WHERE id = :id');
+            $stmt->bindParam(':agent', $agent);
+            $stmt->bindParam(':id', $this->id);
+            $stmt->execute();
+        }
+
+        public function setStatus(PDO $db, string $status) {
+            $this->status = $status;
+
+            $stmt = $stmt = $db->prepare('UPDATE Ticket SET status = :status WHERE id = :id');
+            $stmt->bindParam(':status', $status);
+            $stmt->bindParam(':id', $this->id);
+            $stmt->execute();
+        }
+
+        public function setDepartment(PDO $db, string $department) {
+            $this->department = $department;
+
+            $stmt = $stmt = $db->prepare('UPDATE Ticket SET department = :department WHERE id = :id');
+            $stmt->bindParam(':department', $department);
+            $stmt->bindParam(':id', $this->id);
+            $stmt->execute();
         }
     }
-
 ?>
