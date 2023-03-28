548c0e5e5756a579b35532906d5ee527c4db6ec9
limapedro12 <lima.pedro.0000@gmail.com>
2023-03-28 09:39:32 +0100

    Started templates

---
+++ templates/ticket.tpl.php
@@ -0,0 +1,11 @@
+<?php
+    declare(strict_types = 1); 
+
+    require_once(__DIR__ . '/../database/ticket.class.php');
+    require_once(__DIR__ . '/../database/hashtag.class.php');
+    require_once(__DIR__ . '/../utils/message.php');
+?>
+
+<?php function drawTicket(Ticket $ticket) { ?>
+    
+<?php } ?>
