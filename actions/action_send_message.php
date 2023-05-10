dd2fe507a497e8e89bc96b33e8011d02278394b1
up202108806 <up202108806@edu.fe.up.pt>
2023-05-10 14:03:19 +0100

    Message Ajax almost done

--- actions/action_send_message.php
+++ actions/action_send_message.php
@@ -6,6 +6,12 @@
 
     require_once(__DIR__ . '/../utils/session.php');
     $session = new Session();
+?>
+    <p> <?= $_POST['ticketId'] ?> </p>
+    <p> <?= $_POST['isFromClient'] ?> </p>
+    <p> <?= $_POST['message'] ?> </p>
+
+<?php
 
     Message::createAndAdd($db, intval($_POST['ticketId']), $_POST['isFromClient']=="true", $_POST['message'], $session->getName());
 
--- js/send_message_ajax.js
+++ js/send_message_ajax.js
@@ -10,8 +10,12 @@ function handleEvent() {
   ticket_id = document.location.search.substring(4);
 
   var request = new XMLHttpRequest();
-  request.open("post", "../actions/action_send_message.php", true)
-  request.send(encodeForAjax({ticketId: ticket_id, isFromClient: is_from_client, message: message_text}))
+  request.open("post", "../actions/action_send_message.php", true);
+  let j = encodeForAjax({ticketId: ticket_id, isFromClient: is_from_client, message: message_text});
+  console.log(j);
+  request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  request.send(j);
+  console.log("sent");
 }
 const button = document.querySelector("button.message_button")
 button.addEventListener('click', handleEvent)
