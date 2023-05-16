de8c32038f7fe8dfb390b498c1826d47e744ae9b
pedroojanuu <93834123+pedroojanuu@users.noreply.github.com>
2023-05-16 09:55:16 +0100

    Register security.

--- actions/action_register.php
+++ actions/action_register.php
@@ -12,8 +12,20 @@
   if (User::emailExists($db, $_POST['email'])) {
     $session->addMessage('Register error', 'Email already exists!');
     header('Location: ' . $_SERVER['HTTP_REFERER']);
+  } else if ($_POST['username'] == '' || $_POST['email'] == '' || $_POST['name'] == '' || $_POST['password'] == '') {
+    $session->addMessage('Register error', 'One or more fields are empty!');
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
   } else if (User::usernameExists($db, $_POST['username'])) {
     $session->addMessage('Register error', 'Username already exists!');
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+  } else if (!preg_match("/^[a-zA-Z1-9\s]+$/", $_POST['name'])) {
+    $session->addMessage('Register error', 'Name can only contain letters, numbers and spaces!');
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+  } else if (!preg_match("/^[a-zA-Z1-9_\-\.]+$/", $_POST['username'])) {
+    $session->addMessage('Register error', 'Username can only contain letters, numbers, underscores(_), dots(.) and hyphens(-)!');
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+  } else if (!preg_match("/^[a-zA-Z1-9_\-\.@]+$/", $_POST['email'])) {
+    $session->addMessage('Register error', 'E-mail address invalid!');
     header('Location: ' . $_SERVER['HTTP_REFERER']);
   } else if ($_POST['password'] != $_POST['password2']) {
     $session->addMessage('Register error', 'Passwords do not match!');
