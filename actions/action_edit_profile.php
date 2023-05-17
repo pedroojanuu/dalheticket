ae844850cf0b302f711629ae521d3d7823c67f14
up202108806 <up202108806@edu.fe.up.pt>
2023-05-17 15:08:10 +0100

    Fixed Bug in action_edit_profile.php

--- actions/action_edit_profile.php
+++ actions/action_edit_profile.php
@@ -16,20 +16,17 @@ $logged_user = User::getUserByUsername($db, $session->getName());
 
 $username = $_POST['username'];
 
-if($_POST['field'] != 'name' || $_POST['field'] != 'email'){
-  header('Location: ../index.php');
+if($_POST['field'] != 'name' && $_POST['field'] != 'email'){
+  throw new ErrorException("Tas me a tentar enganar - Field: " . $_POST['field'] . " ...", 0, 1);
   exit();
 }
 
 
 if($session->getName() == $username || $logged_user->type == 'admin' &&
   !(User::emailExists($db, $_POST['email']) || 
-  $_POST['username'] == '' || $_POST['email'] == '' || $_POST['name'] == '' || $_POST['password'] == '' ||
-  User::usernameExists($db, $_POST['username']) || 
+  $_POST['username'] == '' || $_POST['email'] == '' || $_POST['name'] == '' || $_POST['password'] == '' || 
   !preg_match("/^[a-zA-Z1-9\s]+$/", $_POST['name']) ||
-  !preg_match("/^[a-zA-Z1-9_\-\.]+$/", $_POST['username']) ||
-  !preg_match("/^[a-zA-Z1-9_\-\.@]+$/", $_POST['email']) ||
-  $_POST['password'] != $_POST['password2'])) {
+  !preg_match("/^[a-zA-Z1-9_\-\.@]+$/", $_POST['email']))) {
     
   $field = $_POST['field'];
   $value = $_POST['value'];
